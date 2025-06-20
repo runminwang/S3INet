07b0f89a5292400182dacf9efdd83da19cfcad3c
chh <toocomplicated@163.com>
2025-06-20 20:46:17 +0800

    网址

--- eval.sh
+++ eval.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-###### test eval ############
+###### test eval ###################
 ##################### Total-Text ###################################
 CUDA_LAUNCH_BLOCKING=1 python eval_TextPMs_CS_Rate3_merge_fpn_DFFC.py --exp_name Totaltext --checkepoch 250 --test_size 640 1024 --threshold 0.4 --score_i 0.7 --recover watershed --gpu 0 # --viz
 
